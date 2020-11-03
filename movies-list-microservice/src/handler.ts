80a16c18c4b92bca08165adb17766c63d809fd0d
Chinmay Kulkarni <chromonav@gmail.com>
2020-11-03 13:52:40 +0530

    Feat: Implement benchmarking, fix: should not use keys

--- movies-list-microservice/src/handler.ts
+++ movies-list-microservice/src/handler.ts
@@ -2,21 +2,50 @@ import { APIGatewayProxyHandler } from "aws-lambda";
 import "source-map-support/register";
 import * as client from "./utils/redis";
 import * as responseHandler from "./utils/responseHandler";
+import { timer } from "./utils/timeUtils";
 
 export const moviesListHandler: APIGatewayProxyHandler = async (
   _event,
   _context
 ) => {
   try {
-    let movies = await client.scan("movie:*"); // use scan
+    const benchmark = timer("listmovies");
+    let [, movies] = await client.scan([
+      "0",
+      "MATCH",
+      "movie:*",
+      "COUNT",
+      "10000",
+    ]); 
     movies = await Promise.all(
       movies.map(async (movieName) => {
         const movieDetails = await client.hgetall(movieName);
         return movieDetails;
       })
     );
+    benchmark.stop();
     return responseHandler.successMessage({
       data: movies,
+    });
+  } catch (e) {
+    return responseHandler.errorMessage({
+      errorCode: 500,
+      errorMessage: e.message,
+    });
+  }
+};
+
+export const moviesGetHandler: APIGatewayProxyHandler = async (
+  event,
+  _context
+) => {
+  try {
+    const benchmark = timer("getmovie");
+    const movieId: string = event.pathParameters.id;
+    const movie = await client.hgetall(movieId);
+    benchmark.stop();
+    return responseHandler.successMessage({
+      data: movie,
     });
   } catch (e) {
     return responseHandler.errorMessage({
---
+++ movies-list-microservice/src/utils/timeUtils.ts
@@ -0,0 +1,10 @@
+export const timer = function(name) {
+    const start = new Date();
+    return {
+        stop: function() {
+            const end  = new Date();
+            const time = end.getTime() - start.getTime();
+            console.log('Timer:', name, 'finished in', time, 'ms');
+        }
+    }
+};
